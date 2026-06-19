42cc322439da37b7044eb66b57136a7276e01616
Nicolas Wenger <nicolaswenger9@gmail.com>
2026-06-19 10:59:45 -0300

    Opciones de casos de uso y pagina basica de recepcion bolsin

--- PPAI_Front_End/src/app/app.routes.ts
+++ PPAI_Front_End/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Opciones } from './features/bolsines/pages/opciones/opciones';
+import { Recepcion } from './features/bolsines/pages/recepcion/recepcion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Opciones },
+  { path: 'registrar-recepcion', component: Recepcion }
+];
+
