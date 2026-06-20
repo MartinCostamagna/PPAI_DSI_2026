9fd40d426dfe86c4351b693a95802809d0a9d551
Ciro <cirozavattarocampos@gmail.com>
2026-06-19 22:56:26 -0300

    Arrego lo que sobreescribi

--- PPAI_Front_End/src/app/app.routes.ts
+++ PPAI_Front_End/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { Opciones } from './features/bolsines/pages/opciones/opciones';
-import { Recepcion } from './features/bolsines/pages/recepcion/recepcion';
+import { Inicio } from './pages/inicio/inicio';
+import { BolsinesEnviadosAEstaComisionMedica } from './pages/bolsines-enviados-aesta-comision-medica/bolsines-enviados-aesta-comision-medica';
+import { BolsinSeleccionado } from './pages/bolsin-seleccionado/bolsin-seleccionado';
 
 export const routes: Routes = [
-  { path: '', component: Opciones },
-  { path: 'registrar-recepcion', component: Recepcion }
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: Inicio },
+  { path: 'bolsines-enviados-a-esta-comision-medica', component: BolsinesEnviadosAEstaComisionMedica },
+  { path: 'bolsinSeleccionado', component: BolsinSeleccionado }
 ];
-
--- PPAI_Front_End/src/app/features/bolsines/bolsines.routes.ts
+++
@@ -1,4 +0,0 @@
-//export const BOLSINES_ROUTES: Routes = [
-//  { path: '', component: MenuOpcionesPage },
-//  { path: 'registrar-recepcion', component: RegistrarRecepcionPage },
-//];
